ec7109b7b891986494fe7af73fd23b8a87855efd
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-06-02 15:21:52 -0400

    created the first file using randomness to the file name

--- create_bucket.py
+++ create_bucket.py
@@ -24,6 +24,13 @@ def create_bucket_second_way(name, s3_connection):
     return bucket_name, bucket_response
 
 
+def create_temp_file(size, file_name, file_content):
+    random_file_name = ''.join([str(uuid.uuid4().hex[:6]), file_name])
+    with open(random_file_name, 'w') as f:
+        f.write(str(file_content) * size)
+    return random_file_name
+
+
 if __name__ == '__main__':
     # first_bucket_name, first_response = create_bucket(bucket_prefix='firstpythonbucket', s3_connection=s3_resource.meta.client)
     # print(first_response)
@@ -32,5 +39,7 @@ if __name__ == '__main__':
     # second_bucket_name, second_response = create_bucket(bucket_prefix='secondpythonbucket', s3_connection=s3_resource)
     # print(second_response)
 
-    my_bucket_name, my_bucket_response = create_bucket_second_way('first-bucket-boto3', s3_connection=s3_resource.meta.client)
-    print(my_bucket_response)
+    # my_bucket_name, my_bucket_response = create_bucket_second_way('first-bucket-boto3', s3_connection=s3_resource.meta.client)
+    # print(my_bucket_response)
+
+    first_file_name = create_temp_file(300, 'firstfile.txt', 'f')
